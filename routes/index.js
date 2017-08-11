c875ca98f91c372f67a914c1c62a51c2630a33b7
Iván Sánchez <mazyvan@hotmail.com>
2017-08-11 00:01:01 -0500

    tryToRemoveExclamationMarks and tryToRemoveQuestionMarks

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 let Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('uni_automata', 'uni_automata', 'uni_automata123', {
+const sequelize = new Sequelize('check_uni_automata', 'uni_automata', 'uni_automata123', {
   host: 'localhost',
   dialect: 'mysql',
   pool: {
@@ -54,9 +54,10 @@ Type.belongsToMany(Word, { through: 'wordtype' })
 sequelize.sync()
 
 /**
- * @param {Object} sentence 
+ * Regresa un objeto de este tipo {start: pos, end: pos}
+ * @param {Array} sentence 
  * @param {Object} startAt 
- * @return {Object} Regresa un objeto de este tipo {start: pos, end: pos}
+ * @return {Object}
  */
 function tryToGetSintagmaVerbalPositions(sentence, startAt = 0) {
   if (!sentence[startAt].hasData()) return
@@ -68,9 +69,10 @@ function tryToGetSintagmaVerbalPositions(sentence, startAt = 0) {
 }
 
 /**
- * @param {Object} sentence
+ * Regresa un objeto de este tipo {start: pos, end: pos}
+ * @param {Array} sentence
  * @param {Object} startAt 
- * @return {Object} Regresa un objeto de este tipo {start: pos, end: pos}
+ * @return {Object}
  */
 function tryToGetSintagmaNominalPositions(sentence, startAt = 0) {
   if (!sentence[startAt] || !sentence[startAt].hasData()) return
@@ -104,9 +106,10 @@ function tryToGetSintagmaNominalPositions(sentence, startAt = 0) {
 }
 
 /**
- * @param {Object} sentence
+ * Regresa un objeto de este tipo {start: pos, end: pos}
+ * @param {Array} sentence
  * @param {Object} startAt 
- * @return {Object} Regresa un objeto de este tipo {start: pos, end: pos}
+ * @return {Object}
  */
 function tryToGetSintagmaAdjetivalPositions(sentence, startAt = 0) {
   if (!sentence[startAt] || !sentence[startAt].hasData()) return { start: startAt, end: startAt }
@@ -133,9 +136,10 @@ function tryToGetSintagmaAdjetivalPositions(sentence, startAt = 0) {
 }
 
 /**
- * @param {Object} sentence
+ * Regresa un objeto de este tipo {start: pos, end: pos}
+ * @param {Array} sentence
  * @param {Object} startAt 
- * @return {Object} Regresa un objeto de este tipo {start: pos, end: pos}
+ * @return {Object}
  */
 function tryToGetSintagmaAdverbialPositions(sentence, startAt = 0) {
   if (!sentence[startAt] || !sentence[startAt].hasData()) return { start: startAt, end: startAt }
@@ -154,9 +158,10 @@ function tryToGetSintagmaAdverbialPositions(sentence, startAt = 0) {
 }
 
 /**
- * @param {Object} sentence
+ * Regresa un objeto de este tipo {start: pos, end: pos}
+ * @param {Array} sentence
  * @param {Object} startAt 
- * @return {Object} Regresa un objeto de este tipo {start: pos, end: pos}
+ * @return {Object}
  */
 function tryToGetSintagmaPreposicionalPositions(sentence, startAt = 0) {
   if (!sentence[startAt] || !sentence[startAt].hasData()) return { start: startAt, end: startAt }
@@ -173,9 +178,10 @@ function tryToGetSintagmaPreposicionalPositions(sentence, startAt = 0) {
 }
 
 /**
- * @param {Object} sentence
+ * Regresa un objeto de este tipo {start: pos, end: pos}
+ * @param {Array} sentence
  * @param {Object} startAt 
- * @return {Object} Regresa un objeto de este tipo {start: pos, end: pos}
+ * @return {Object}
  */
 function tryToGetAdyacentePositions(sentence, startAt = 0) {
   if (!sentence[startAt] || !sentence[startAt].hasData()) return { start: startAt, end: startAt }
@@ -193,14 +199,47 @@ function tryToGetAdyacentePositions(sentence, startAt = 0) {
   return { start: startAt, end: startAt }
 }
 
+/**
+ * Regresa un objeto con la oración sin los signos de exclamación
+ * @param {String} sentence
+ * @return {Object}
+ */
+function tryToRemoveExclamationMarks(sentence) {
+  if (sentence[0] != '¡' && sentence[sentence.length - 1] != '!') return { sentence: sentence }
+  if (sentence.length >= 2) {
+    if (sentence[0] == '¡' && sentence[sentence.length - 1] != '!') return { error: 'La oración exclamativa no termina con el signo "!"', sentence: sentence }
+    if (sentence[0] != '¡' && sentence[sentence.length - 1] == '!') return { error: 'La oración exclamativa no comienza con el signo "¡"', sentence: sentence }
+    let invalidStarts = /^(¡que|¡como|¡quien|¡por que|¡cuando|¡donde|¡a quien|¡cuanto)\b/
+    if (invalidStarts.test((sentence.split(' ')[0] + ' ' + sentence.split(' ')[1]).toLowerCase())) return { error: 'La oración exclamativa es incorrecta', sentence: sentence }    
+    return { sentence: sentence.slice(1, sentence.length - 1) }
+  } else return { error: 'La oración exclamativa es incorrecta', sentence: sentence }
+}
+
+/**
+ * Regresa un objeto con la oración sin los signos de interrogación
+ * @param {String} sentence
+ * @return {Object}
+ */
+function tryToRemoveQuestionMarks(sentence) {
+  if (sentence[0] != '¿' && sentence[sentence.length - 1] != '?') return { sentence: sentence }
+  if (sentence.length >= 2) {
+    if (sentence[0] == '¿' && sentence[sentence.length - 1] != '?') return { error: 'La oración interrogativa no termina con el signo "?"', sentence: sentence }
+    if (sentence[0] != '¿' && sentence[sentence.length - 1] == '?') return { error: 'La oración interrogativa no comienza con el signo "¿"', sentence: sentence }
+    let invalidStarts = /^(¿que|¿como|¿quien|¿porque|¿por que|¿porqué|¿cuando|¿donde|¿a quien|¿cuanto|¿para que)\b/
+    if (invalidStarts.test((sentence.split(' ')[0] + ' ' + sentence.split(' ')[1]).toLowerCase())) return { error: 'La oración interrogativa es incorrecta', sentence: sentence }
+    return { sentence: sentence.slice(1, sentence.length - 1) }
+  } else return { error: 'La oración interrogativa es incorrecta', sentence: sentence }
+}
 
 
 
 /**
+ * Compara y regresa un array de sintagmas
  * @param {String} sentence 
- * @return Compara y regresa un array de sintagmas
+ * @return {Array}
  */
 function checkSentence(sentence, lastCheckedPosition = 0, detectedPartsOfSentence = []) {
+
   if (result = tryToGetSintagmaNominalPositions(sentence, lastCheckedPosition)) {
     if (detectedPartsOfSentence.slice(-1)[0] == 'SN') return { error: 'Una oración no puede tener 2 sintagmas nominales juntos' }
     detectedPartsOfSentence.push('SN')
@@ -253,7 +292,12 @@ function checkSentence(sentence, lastCheckedPosition = 0, detectedPartsOfSentenc
 router.get('/', function (req, res, next) {
   let sentence = req.query.sentence
   let words = []
-  if (!sentence) return res.render('index')
+  let error
+  if (!sentence) return res.render('index');
+
+  ({ sentence, error } = tryToRemoveExclamationMarks(sentence))
+  if (!error) ({ sentence, error } = tryToRemoveQuestionMarks(sentence))
+
   let numOfWords = sentence.split(' ').length
   let foundedWords = 0
   sentence.split(' ').forEach((word, index) => {
@@ -298,11 +342,13 @@ router.get('/', function (req, res, next) {
         // Si no se logró analizar toda la oración
         if (sentenceChecked.lastCheckedPosition != words.length) sentenceChecked.parts = []
 
+
+        console.log('el error es ', error);
         res.render('index', {
           sentence: req.query.sentence,
           words: words,
           sintagmas: sentenceChecked.parts,
-          error: (sentenceChecked.error) ? sentenceChecked.error : undefined
+          error: error || ((sentenceChecked.error) ? sentenceChecked.error : undefined)
         })
       }
     })
